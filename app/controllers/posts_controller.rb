5a7133611afc76463ef13f4e99d3a76a8b6b59c0
Max Gordon <gordonmaxc@gmail.com>
2015-02-20 15:08:11 -0800

    delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,13 @@ class PostsController < ApplicationController
     render json:post
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    head :no_content
+    
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :username, :link, :description)
