c5fb69f9fa62869498e248be6d9e1ca4f2a48c8d
Max Gordon <gordonmaxc@gmail.com>
2015-02-20 15:03:04 -0800

    post show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     render json: Post.all
   end
+
+  def show
+    render json: Post.find(params[:id])
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    render json:post
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    render json:post
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :username, :link, :description)
+  end
 end
