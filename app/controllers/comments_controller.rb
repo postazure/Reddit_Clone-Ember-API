6b788672f61f653f088f3723f84e5a47cd4275dd
Max Gordon <gordonmaxc@gmail.com>
2015-02-21 11:49:01 -0800

    add comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,15 @@ class CommentsController < ApplicationController
   def index
     render json: Comment.all
   end
+
+  def create
+    comment = Comment.new(comment_params)    
+    comment.save
+    render json: comment
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:post_id, :username, :body)
+  end
 end
